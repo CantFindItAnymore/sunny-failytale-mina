d01633d118bc4cc287740067c572b8a4d7a10f97
ranjun <1069811427@qq.com>
2020-04-04 21:45:09 +0800

    add home

---
+++ app/pages/home/index.js
@@ -0,0 +1,14 @@
+import { classifyList, rainbowList, outfitList } from '../../utils/common-json/home'
+
+Page({
+
+  data: {
+    classifyList,
+    rainbowList,
+    outfitList
+  },
+
+  onLoad: function (options) {
+
+  }
+})
---
+++ app/utils/common-json/home.js
@@ -0,0 +1,77 @@
+const classifyList = [
+  {
+    name: '裙装',
+    src: '/imgs/skirt.png'
+  },
+  {
+    name: '毛衫',
+    src: '/imgs/sweater.png'
+  },
+  {
+    name: '外套',
+    src: '/imgs/coat.png'
+  },
+  {
+    name: '裤装',
+    src: '/imgs/trousers.png'
+  },
+  {
+    name: '卫衣',
+    src: '/imgs/hoodie.png'
+  },
+  {
+    name: '衬衫',
+    src: '/imgs/shirt.png'
+  },
+  {
+    name: 'T恤',
+    src: '/imgs/Tshirt.png'
+  },
+  {
+    name: '鞋装',
+    src: '/imgs/shoe.png'
+  }
+]
+
+const rainbowList = [
+  {
+    src: '/imgs/rainbow.png',
+    name: '桑尼女童',
+    desc: '80cm - 100cm'
+  },
+  {
+    src: '/imgs/rainbow.png',
+    name: '桑尼女孩',
+    desc: '100cm - 160cm'
+  },
+  {
+    src: '/imgs/rainbow.png',
+    name: '桑尼小绅士',
+    desc: '90cm - 160cm'
+  },
+  {
+    src: '/imgs/rainbow.png',
+    name: '桑尼爱宠'
+  },
+]
+
+const outfitList = [
+  {
+    src: '/imgs/outfit1.png'
+  },
+  {
+    src: '/imgs/outfit2.png'
+  },
+  {
+    src: '/imgs/outfit3.png'
+  },
+  {
+    src: '/imgs/outfit4.png'
+  }
+]
+
+export {
+  classifyList,
+  rainbowList,
+  outfitList
+}
