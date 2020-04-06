056785da39c8b382843aae6456027f2a943a173b
ranjun <1069811427@qq.com>
2020-04-07 01:26:59 +0800

    add my

---
+++ app/utils/common-json/my.js
@@ -0,0 +1,45 @@
+const list = [
+  {
+    name: '客服中心',
+    goal: 'service',
+    src: '/imgs/service.png'
+  },
+  {
+    name: '地址管理',
+    goal: 'address',
+    src: '/imgs/address.png'
+  },
+  {
+    name: '个人资料',
+    goal: 'person',
+    src: '/imgs/person.png'
+  },
+  {
+    name: '直播链接',
+    goal: 'live',
+    src: '/imgs/live.png'
+  }
+]
+
+const my = [
+  {
+    name: '订单',
+    src: '/imgs/order.png',
+    goal: 'order',
+  },
+  {
+    name: '卡包',
+    src: '/imgs/card.png',
+    goal: 'card',
+  },
+  {
+    name: '我的收藏',
+    src: '/imgs/collect.png',
+    goal: 'collect',
+  }
+]
+
+export {
+  list,
+  my
+}
